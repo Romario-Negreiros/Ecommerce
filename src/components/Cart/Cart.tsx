99c93eb54fe538cf4ced5ce0af002fd779d76f11
Romario Negreiros <nromario482@gmail.com>
2021-08-09 16:37:47 -0300

    styled cart components + created prop interface

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,36 +1,73 @@
-import { FC } from 'react';
+import { FC } from "react";
+import Props from "./interfaces/Props";
 import {
-    Container,
-    Summary,
-    SummaryItem,
-    LinkTag,
-    Infos,
-    Options
-} from './styles';
+  Container,
+  Summary,
+  SummaryItem,
+  Infos,
+  Options,
+  SummaryButton,
+  LinkTag,
+  CartList,
+  CartItem,
+  ItemInfo,
+  Title,
+  Category,
+  Price,
+  Quantity,
+  ManageItem,
+  ManagerButton,
+  QuantityButton
+} from "./styles";
 
-const Cart: FC = () => {
-    return (
-        <Container>
-            <Summary>
-            <Infos>
-                <SummaryItem>
-                    hello world
-                </SummaryItem>
-                <SummaryItem>
-                    price: 29389238
-                </SummaryItem>
-                <SummaryItem>
-                    total items: 3023
-                </SummaryItem>
-            </Infos>
-            <Options>
-                <LinkTag to="/">
-                    Go to checkout
-                </LinkTag>
-            </Options>
-            </Summary>
-        </Container>
-    )
-}
+const Cart: FC<Props> = ({ products }) => {
+
+
+  
+  return (
+    <Container>
+      <Summary>
+        <Infos>
+          <SummaryItem>hello world</SummaryItem>
+          <SummaryItem>price: 29389238</SummaryItem>
+          <SummaryItem>total items: 3023</SummaryItem>
+        </Infos>
+        <Options>
+          <SummaryButton>Delete all</SummaryButton>
+          <LinkTag to="/checkout" style={{background: '#2bdc0a'}}>
+            Checkout
+          </LinkTag>
+        </Options>
+      </Summary>
+      <CartList>
+        {products.map((product) => {
+          const { id, title, price, category, quantity } = product;
+          return (
+            <CartItem key={id}>
+              <ItemInfo>
+                <Title>{title}</Title>
+                <Category>
+                  Category:{" "}
+                  {category.charAt(0).toUpperCase() + category.substring(1)}
+                </Category>
+                <Price>$ {(price * quantity).toFixed(2)}</Price>
+                <Quantity>Quantity : {quantity}</Quantity>
+              </ItemInfo>
+              <ManageItem>
+                <ManagerButton style={{background: '#f70029'}}>Delete</ManagerButton>
+                <QuantityButton>
+                  More one
+                </QuantityButton>
+                <QuantityButton>
+                  Less one
+                </QuantityButton>
+              </ManageItem>
+            </CartItem>
+          );
+        })}
+      </CartList>
+    </Container>
+  );
+};
 
 export default Cart;
--- src/components/Cart/styles.ts
+++ src/components/Cart/styles.ts
@@ -1,49 +1,185 @@
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.section`
-    background: ${({theme: { backgrounds }}) => backgrounds.gray };
-    padding: 10px;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  background: ${({ theme: { backgrounds } }) => backgrounds.gray};
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media screen and (min-width: 700px) {
+    flex-direction: row;
+    align-items: initial;
+  }
 `;
 
 export const Summary = styled.div`
-    margin-bottom: 20px;
-    width: 100%;
-    background: #fff;
+  margin-bottom: 20px;
+  width: 100%;
+  background: #fff;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 2px;
+  @media screen and (min-width: 700px) {
+    width: fit-content;
     padding: 20px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    height: fit-content;
+    margin: 0;
+  }
 `;
 
-
-export const Infos = styled.div`
-
-`;
+export const Infos = styled.div``;
 
 export const SummaryItem = styled.p`
-    font-weight: 700;
-    font-size: 1.2rem;
-    margin-top: 5px;
+  font-weight: 700;
+  font-size: 1.2rem;
+  margin-top: 5px;
 `;
 
-export const Options = styled.div`
-    margin-top: 30px;
+export const SummaryButton = styled.button`
+  margin-top: 5px;
+  font-size: 1.1rem;
+  color: #000;
+  padding: 10px 15px;
+  background: ${({ theme: { common } }) => common.cyan};
+  cursor: pointer;
+  transition: opacity 0.4s;
+  border: 0 none;
+  :hover {
+    opacity: 0.5;
+  }
+  @media screen and (min-width: 700px) {
+    width: 100%;
+  }
 `;
 
 export const LinkTag = styled(Link)`
-font-weight: 700;
-font-size: 1.1rem;
-    color: #000;
-    padding: 7px 15px;
-    background: ${({theme: { common }}) => common.cyan};
-    transition: opacity .4s;
-    :hover {
-        opacity: 0.5;
-    }
+  margin-top: 5px;
+  font-size: 1.1rem;
+  cursor: pointer;
+  color: #000;
+  font-weight: bold;
+  text-align: center;
+  padding: 10px 15px;
+  background: ${({ theme: { common } }) => common.cyan};
+  transition: opacity 0.4s;
+  border: 0 none;
+  :hover {
+    opacity: 0.5;
+  }
+  @media screen and (min-width: 700px) {
+    width: 100%;
+  }
 `;
 
+export const Options = styled.div`
+  margin-top: 30px;
+  flex-basis: 100%;
+  display: flex;
+  flex-flow: row wrap;
+  gap: 10px;
+  justify-content: space-between;
+  align-items: center;
+  @media screen and (min-width: 700px) {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+`;
+
+export const CartList = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  @media screen and (min-width: 700px) {
+    width: fit-content;
+    margin-left: 15px;
+    display: grid;
+    gap: 15px;
+    grid-template-columns: 1fr 1fr;
+  }
+  @media screen and (min-width: 1000px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  @media screen and (min-width: 1400px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+export const CartItem = styled.li`
+  align-self: stretch;
+  margin-top: 15px;
+  background: #fff;
+  border-radius: 2px;
+  padding: 10px;
+  @media screen and (min-width: 700px) {
+    margin: 0;
+    display: flex;
+    flex-flow: row wrap;
+  }
+`;
+
+export const ItemInfo = styled.div`
+  margin-bottom: 20px;
+  width: 100%;
+`;
+
+export const Title = styled.h2`
+  font-weight: 900;
+  font-size: 1.4rem;
+  margin-bottom: 5px;
+`;
+
+export const Price = styled.p`
+  font-weight: 800;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+`;
+
+export const Category = styled.p`
+  font-weight: 800;
+  font-size: 1.2rem;
+  margin-bottom: 10px;
+`;
+
+export const Quantity = styled.p`
+  font-weight: 800;
+  font-size: 1.2rem;
+`;
+
+export const ManageItem = styled.div`
+  width: 100%;
+  align-self: flex-end;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-between;
+`;
+
+export const ManagerButton = styled.button`
+  font-size: 1.2rem;
+  color: #fff;
+  cursor: pointer;
+  width: 100%;
+  padding: 10px 15px;
+  background: ${({ theme: { common } }) => common.cyan};
+  transition: opacity 0.4s;
+  border: 0 none;
+  :hover {
+    opacity: 0.5;
+  }
+`;
+
+export const QuantityButton = styled.button`
+  font-size: 1.2rem;
+  margin-top: 10px;
+  color: #000;
+  cursor: pointer;
+  width: 48%;
+  padding: 10px 15px;
+  background: ${({ theme: { common } }) => common.cyan};
+  transition: opacity 0.4s;
+  border: 0 none;
+  :hover {
+    opacity: 0.5;
+  }
+`;
