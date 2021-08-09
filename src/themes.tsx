1f1269b177e979fe913489876db17bbdbf504150
Romario Negreiros <nromario482@gmail.com>
2021-08-09 16:38:41 -0300

    added new colors

--- src/themes.tsx
+++ src/themes.tsx
@@ -1,14 +1,14 @@
 const themes = {
-    fonts: {
-        white: '#fff',
-    },
     backgrounds: {
         gray: '#666666',
         headerbg: '#20232a',
-        underheader: '#282c34'
+        underheader: '#282c34',
+        notification: '#f02849'
     },
     common: {
-        cyan: '#61dafb'
+        cyan: '#61dafb',
+        red: '#f70029',
+        green: '#2bdc0a'
     }
 }
 
