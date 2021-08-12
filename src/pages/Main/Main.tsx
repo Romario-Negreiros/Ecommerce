1b96c9b31ff908f5f8236866a49628e683f2b12b
Romario Negreiros <nromario482@gmail.com>
2021-08-12 09:46:23 -0300

    imported products + added new route to products component

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,6 +1,6 @@
 import { FC, useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Home, Cart, Checkout } from '../../components/index';
+import { Home, Cart, Checkout, Products} from '../../components/index';
 import { Header, Searcher } from './components/index';
 import { Error, IsLoading } from './styles';
 import Product from './interfaces/productsInterface';
@@ -44,6 +44,9 @@ const Main: FC = () => {
             </Route>
             <Route path="/checkout">
               <Checkout productsOnCart={productsOnCart as Product[] | null} />
+            </Route>
+            <Route path="/products">
+              <Products products={products as Product[]} setProductsOnCart={setProductsOnCart} />
             </Route>
           </Switch>
         </>
